f23677940737e9c13bb4c522664cb48b9289a579
Alex Hanna <ahanna0608@gmail.com>
2023-06-01 15:43:17 +0000

    Primitive Movement

--- snake.py
+++ snake.py
@@ -1,17 +1,42 @@
 from tkinter import *
-from pixel import Pixel
-class Snake():
+from tkinter import ttk
+from enum import Enum
+
+class Snake(Frame):
     def __init__(self, parent):
-        self.pixels = [Pixel(parent, 1, 1), Pixel(parent, 2, 1)]
-    def moveUp(self):
-        for pixel in self.pixels: 
-            pixel.place(y=pixel.winfo_y() - 16)
-    def moveLeft(self):
-        for pixel in self.pixels: 
-            pixel.place(x=pixel.winfo_x() - 16)
-    def moveDown(self):
-        for pixel in self.pixels: 
-            pixel.place(y=pixel.winfo_y() + 16)
-    def moveRight(self):
-        for pixel in self.pixels: 
-            pixel.place(x=pixel.winfo_x() + 16)
+        self.direction = Direction.DOWN
+        self.speed = 500
+        super().__init__(master=parent, background="lime", width=25, height=25)
+        self.place(x=0, y=0)
+    def set_direction(self, direction):
+        self.direction = direction
+    def move(self):
+        if self.direction == Direction.UP:
+            self.place(y=self.winfo_y()-25)
+        elif self.direction == Direction.DOWN:
+            self.place(y=self.winfo_y()+25)
+        elif self.direction == Direction.LEFT:
+            self.place(x=self.winfo_x()-25)
+        elif self.direction == Direction.RIGHT:
+            self.place(x=self.winfo_x()+25)
+        root.after(snake.speed, snake.move)
+
+class Direction(Enum):
+    UP = 0
+    DOWN = 1
+    LEFT = 2
+    RIGHT = 3
+
+root = Tk()
+frame = Frame(root, background="black", width=250, height=250)
+frame.pack()
+
+snake = Snake(frame)
+
+root.bind('<Key-w>', lambda e: snake.set_direction(Direction.UP))
+root.bind('<Key-a>', lambda e: snake.set_direction(Direction.LEFT))
+root.bind('<Key-s>', lambda e: snake.set_direction(Direction.DOWN))
+root.bind('<Key-d>', lambda e: snake.set_direction(Direction.RIGHT))
+
+root.after(snake.speed, snake.move)
+root.mainloop()
