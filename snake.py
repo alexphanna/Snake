9b8ef27113cde076de5ee078da9fb134d53d66f3
alexphanna <ahanna0608@gmail.com>
2023-06-06 15:55:21 -0400

    Bug fixes

--- snake.py
+++ snake.py
@@ -10,7 +10,7 @@ class Snake(list):
         self.moved = False
         self.alive = True
         self.master = master
-        front = Frame(master=self.master, background="lime", width=Config.PIXEL_SIZE, height=Config.PIXEL_SIZE)
+        front = Frame(master=self.master, background="lime", width=Config.PIXEL_SIZE, height=Config.PIXEL_SIZE, name="front")
         front.grid(row=1, column=1)
         self.append(front)
         self.grow()
@@ -25,13 +25,6 @@ class Snake(list):
     def move(self):
         if self.direction == Direction.UP or self.direction == Direction.DOWN:
             row = self[0].grid_info()['row'] + (int)(self.direction.value / abs(self.direction.value))
-            for frame in self.master.winfo_children():
-                if frame.grid_info()['row'] == row and frame.grid_info()['column'] == self[0].grid_info()['column']:
-                    if isinstance(frame, Apple):
-                        frame.grid_remove()
-                        Apple(self.master)
-                    else:
-                        self.alive = False
             if row == Config.GRID_ROWS:
                 self[0].grid(row = 0)
             elif row == -1:
@@ -40,19 +33,20 @@ class Snake(list):
                 self[0].grid(row = row)
         elif self.direction == Direction.RIGHT or self.direction == Direction.LEFT:
             column = self[0].grid_info()['column'] + (int)(self.direction.value / abs(self.direction.value))
-            for frame in self.master.winfo_children():
-                if frame.grid_info()['column'] == column and frame.grid_info()['row'] == self[0].grid_info()['row']:
-                    if isinstance(frame, Apple):
-                        frame.grid_remove()
-                        Apple(self.master)
-                    else:
-                        self.alive = False
             if column == Config.GRID_COLUMNS:
                 self[0].grid(column = 0)
             elif column == -1:
                 self[0].grid(column = Config.GRID_COLUMNS - 1)
             else:
                 self[0].grid(column = column)
+        for frame in self.master.winfo_children():
+            if frame.grid_info() == self[0].grid_info():
+                if isinstance(frame, Apple):
+                    frame.grid_remove()
+                    self.grow()
+                    Apple(self.master)
+                elif frame.winfo_name() != "front":
+                    self.alive = False
 
         index = len(self) - 1
         while index > 0:
